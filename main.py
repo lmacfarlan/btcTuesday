ac4ae93d02b9ccd40553172f8785e95c864fe9a9
lmacfarlan <luke@metismachine.com>
2018-06-18 16:56:38 -0400

    update for testing

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 #   Here we provide a template to precit bitcoin price and deploy   #
 #   at scale on a user-defined schedule, taking advantage of        #
 #   Metis Machine curated data, and external 3rd party data.        #
+#   Attempting to use VASI for the first time #
+#
 
 ## Import some needed dependencies
 import os
